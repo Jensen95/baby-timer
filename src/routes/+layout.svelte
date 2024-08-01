2e9ac34d7df28b44aabfc6673d903febbc4e963c
Morten Jensen <mje@monthio.com>
2024-08-01 15:40:56 +0200

    Adds bulma and mdx support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,22 @@
 <script>
+	import '../app.scss';
 	import Header from './Header.svelte';
 	import '../app.css';
 </script>
 
 <div class="app">
-	<Header />
+	<Header></Header>
 
 	<main>
-		<slot />
+		<slot></slot>
 	</main>
 
 	<footer>
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
+
+<slot></slot> 
 
 <style>
 	.app {
